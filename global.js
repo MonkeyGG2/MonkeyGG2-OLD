9cdafbe2ba84ac75445d60d2b310ace35105d220
BluePotato102 <bluepotato102@noreply.codeberg.org>
2022-10-06 00:41:29 +0200

    Update 'global.js'

--- global.js
+++ global.js
@@ -15,35 +15,18 @@ function makeclone(){
         iframe.src = url.toString();
         win.document.body.appendChild(iframe);
         window.location.replace(localStorage.getItem("cloneURL"));
+        alert('test');
         window.close();
     }
 }
 
-function addbutton(){
-    const btn = document.createElement("button");
-    btn.innerText = "Back";
-    btn.id="go-back";
-    btn.onclick=function(){window.location.href='/';};
-
-    btn.style.cssText= "box-sizing:inherit;position:absolute;z-index:9999;left:0;width:75px;height:50px;border-radius:0290486px290486px0;color:#000;padding:010px;line-height:50px;min-height:50px;border:none;border-bottom:3pxsolid#b5b5b5;will-change:transform;animation:bounceY2s.5s;transition:transform.5scubic-bezier(.55,0,.1,1);display:flex;align-items:center;justify-content:center;background:#1ec2e5;top:51px;"
-
-    const tab = document.createElement("button");
-    tab.id="tab";
-    tab.style.cssText = "background-color:blue;border-radius: 290486px 0 0 290486px;cursor: pointer; z-index: 9999; color: #000; height: 50px; padding: 19px 10px; border: none;";
-    tab.style.width="15px";
-    document.body.appendChild(tab);
-    document.body.appendChild(btn);
-
-    function drag(e) {
-    tab.style.transform = `translate(${e.pageX - 20}px, ${e.pageY - 10}px)`;
-    btn.style.transform = `translate(${e.pageX - 20}px, ${e.pageY - 10}px)`;
-    }
-    tab.addEventListener("mousedown", () =>
-    document.addEventListener("mousemove", drag)
-    );
-    tab.addEventListener("mouseup", () =>
-    document.removeEventListener("mousemove", drag)
-    );
+function askclone(){
+    if (confirm("Would you like to clone now?")){
+        if(!(window.top.location.href=="about:blank")){
+            makeclone()
+        }else{
+            alert("You are already in an about:blank page")
+        }
 }
 
 if (localStorage.getItem("cloneURL") == null) {
@@ -51,15 +34,12 @@ if (localStorage.getItem("cloneURL") == null) {
 }
 
 if (localStorage.getItem("clone") == null) {
-    localStorage.setItem("clone", confirm("Do want auto cloak?\n\nif you don't remember you can still press ctrl q\n\nnote: we will make a settings page later where this can be changed"));
-    clone();
+    localStorage.setItem("clone", confirm("Do want auto cloak?\n\nif you don't remember you can still press ctrl q\n\nnote: you can always change this and more in settings"));
+    askclone();
 } else if (localStorage.getItem("clone") == 'true') {
     makeclone();
 }
 
-if (!(window.location.href=="/")){
-    addbutton();
-}
 
 document.addEventListener('keydown', (e) => {
     if (e.key.toLowerCase() === 'q' && e.ctrlKey) {
