1a37a37a742cc8bb1eb2f7c6745c8af2bd09080c
BluePotato102 <105396427BluePotato102@users.noreply.github.com>
2022-12-25 14:18:58 -0600

    smash karts

---
+++ games/smash-karts/service-worker2.js
@@ -0,0 +1,27 @@
+const PRECACHE = 'precache-v1';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  return null;
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  return null;
+});
