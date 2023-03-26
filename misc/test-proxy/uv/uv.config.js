0b69182e3341028dbfd04a61e5f181a17548dabb
MonkeyGG2 <spinosaurus882@gmail.com>
2023-03-26 12:37:06 -0400

    added proxy stuff

---
+++ misc/test-proxy/uv/uv.config.js
@@ -0,0 +1,13 @@
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+  prefix: "",
+  bare: "https://uv.holyubofficial.net/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "uv.handler.js",
+  client: "uv.client.js",
+  bundle: "uv.bundle.js",
+  config: "uv.config.js",
+  sw: "uv.sw.js",
+};
